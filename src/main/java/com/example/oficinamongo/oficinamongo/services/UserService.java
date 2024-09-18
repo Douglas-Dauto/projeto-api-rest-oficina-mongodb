5230f69198458fc6147660b0cd95af6f1bacf3cd
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-18 15:58:56 -0300

    Implementado busca de usuário por id e feito tratamento de exceções

--- src/main/java/com/example/oficinamongo/oficinamongo/services/UserService.java
+++ src/main/java/com/example/oficinamongo/oficinamongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.oficinamongo.oficinamongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.oficinamongo.oficinamongo.domain.User;
 import com.example.oficinamongo.oficinamongo.repositories.UserRepository;
+import com.example.oficinamongo.oficinamongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado."));
     }
 }
