35ffcde4f5457d2a59da0c95e42bc59c828576d8
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-18 11:03:24 -0300

    Primeira versão

---
+++ src/main/java/com/example/oficinamongo/oficinamongo/OficinamongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oficinamongo.oficinamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oficinamongo/oficinamongo/OficinamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oficinamongo.oficinamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
