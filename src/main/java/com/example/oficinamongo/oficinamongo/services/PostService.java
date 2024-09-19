adfbe0427dfb80918bf7bb79515d65a0525af9a4
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-19 20:06:21 -0300

    Implementado busca por um post

---
+++ src/main/java/com/example/oficinamongo/oficinamongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.oficinamongo.oficinamongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.oficinamongo.oficinamongo.domain.Post;
+import com.example.oficinamongo.oficinamongo.repositories.PostRepository;
+import com.example.oficinamongo.oficinamongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> user = postRepository.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado."));
+    }
+}
