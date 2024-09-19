31626af9cfd840bafe801b93cb3d10e4caff0996
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-19 14:08:32 -0300

    Implementado posts de usuário

--- src/main/java/com/example/oficinamongo/oficinamongo/config/Instantiation.java
+++ src/main/java/com/example/oficinamongo/oficinamongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.example.oficinamongo.oficinamongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.oficinamongo.oficinamongo.domain.Post;
 import com.example.oficinamongo.oficinamongo.domain.User;
+import com.example.oficinamongo.oficinamongo.repositories.PostRepository;
 import com.example.oficinamongo.oficinamongo.repositories.UserRepository;
 
 @Configuration
@@ -14,14 +18,25 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post("Vou viajar para Austrália. Abraços!", sdf.parse("21/03/2024"), null, "Partiu viagem", maria);
+        Post post2 = new Post("Acordei feliz hoje!", sdf.parse("12/05/2024"), null, "Bom dia", maria);
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
     }
 }
---
+++ src/main/java/com/example/oficinamongo/oficinamongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.oficinamongo.oficinamongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.oficinamongo.oficinamongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
