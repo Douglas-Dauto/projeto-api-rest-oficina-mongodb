11a75752d354f884b4e665f4a21007135aea2fdf
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-20 13:39:48 -0300

    Implementado consulta com query params

--- src/main/java/com/example/oficinamongo/oficinamongo/services/PostService.java
+++ src/main/java/com/example/oficinamongo/oficinamongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.oficinamongo.oficinamongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class PostService {
         Optional<Post> user = postRepository.findById(id);
 
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado."));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
