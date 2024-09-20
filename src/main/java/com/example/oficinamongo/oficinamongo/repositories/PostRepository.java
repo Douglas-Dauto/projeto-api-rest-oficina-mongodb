e915013b91c434f253790dc4e92d58d189aea7c7
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-20 13:56:24 -0300

    Implementado consulta de titulo do post com @query

--- src/main/java/com/example/oficinamongo/oficinamongo/repositories/PostRepository.java
+++ src/main/java/com/example/oficinamongo/oficinamongo/repositories/PostRepository.java
@@ -3,11 +3,14 @@ package com.example.oficinamongo.oficinamongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.oficinamongo.oficinamongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
